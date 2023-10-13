f444b69c50d540b2ed6ae24c9dca488e4148cd69
TobyMeller <120049117+TobyMeller@users.noreply.github.com>
2023-10-13 19:00:39 +0100

    updated to make context the correct output
    to do:
    
    1)make it so user input for direction and just output 1
    2) sort rounding

--- src/pyth_API_proxy.js
+++ src/pyth_API_proxy.js
@@ -18,42 +18,90 @@ async function handleRequest(request) {
   try {
     const symbol = 'Crypto.BTC/USD';
     const price = await getPrice();
+
+    //Eth mainnet Contract addresses for test
+    const BTC_token_address = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+    const USDT_token_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+
     // Calculate fake bid and ask
     const fakeBid = price + (price * 0.001);
     const fakeAsk = price - (price * 0.001);
 
+    function roundToDecimalPlaces(number, decimalPlaces) {
+      const multiplier = 10 ** decimalPlaces;
+      return Math.round(number * multiplier) / multiplier;
+    }
+
+    const fakeAskInverted = 1 / fakeAsk;
+    const fakeAskInvertedrounded = roundToDecimalPlaces(fakeAskInverted, 18);
+
+    console.log('Inverted:', fakeAskInverted)
+    console.log('rounded:',fakeAskInvertedrounded )
+    
+    //Amount limits
+    const AmountBuy = 10;
+    const AmountSell = 10 * price;
+
     // Calculate expiry (current time + 1 minute)
     const now = new Date(); // Get the current date and time
-    const expiry = Math.floor((now.getTime() + 1 * 60 * 1000) / 1000);   
+    const expiry = Math.floor((now.getTime() + 1 * 60 * 1000) / 1000);
+    console.log('expiry:', expiry)
     
     // Sign the data with your EVM wallet's private key
     const privateKey = '1c8ee97c2a1c154e55c4362a18a0e0b62e7cbd64b88b42f15742f3e3dc2c1e91'; // Replace with your actual private key
+
+    //convert address from 20 to 32
+    const BTC_addressto32 = ethers.utils.zeroPad(ethers.utils.arrayify(BTC_token_address), 32);
+    console.log('addressto32:', BTC_addressto32);
+
+    const USDT_addressto32 = ethers.utils.zeroPad(ethers.utils.arrayify(USDT_token_address), 32);
+    console.log('addressto32:', USDT_addressto32);
     
-    const context = [
-      ethers.utils.formatBytes32String(symbol).toString(),
-      ethers.utils.parseEther(price.toString()).toString(),
+
+    const contextBuy = [
+      ethers.utils.solidityKeccak256(["address", "address"],[BTC_token_address, USDT_token_address]).toString(),
       ethers.utils.parseEther(fakeBid.toString()).toString(),
-      ethers.utils.parseEther(fakeAsk.toString()).toString(),
+      ethers.utils.parseEther(AmountBuy.toString()).toString(),
       BigNumber.from(expiry).toString()
     ]
 
-    console.log = ('context :', context)
+    console.log('context :', contextBuy)
 
-  const signer = new ethers.Wallet(privateKey);
-  const signature = await signer.signMessage(ethers.utils.arrayify(ethers.utils.solidityKeccak256(
-    ["uint256", "uint256", "uint256", "uint256", "uint256"],
-    context
-  )));
+    const contextSell = [
+      ethers.utils.solidityKeccak256(["address", "address"],[USDT_token_address, BTC_token_address]).toString(),
+      ethers.utils.parseEther(fakeAskInvertedrounded.toString()).toString(),
+      ethers.utils.parseEther(AmountSell.toString()).toString(),
+      BigNumber.from(expiry).toString()
+    ]
+
+    console.log('context :', contextSell)
+
+    const signer = new ethers.Wallet(privateKey);
+    const signatureBuy = await signer.signMessage(ethers.utils.arrayify(ethers.utils.solidityKeccak256(
+      ["uint256", "uint256", "uint256","uint256" ],
+      contextBuy
+    )));
+
+    const signatureSell = await signer.signMessage(ethers.utils.arrayify(ethers.utils.solidityKeccak256(
+      ["uint256", "uint256", "uint256", "uint256"],
+       contextSell
+    )));
 
     // Create the SignedContextV1Struct
-    const SignedContextV1Struct = {
+    const SignedContextV1StructBuy = {
       signer: '0x0124555E401547219fB024aE5F8C5101c6f7Cb24', // Replace with your signer's address
-      signature: signature,
-      context: context
+      signature: signatureBuy,
+      context: contextBuy
+    };
+
+    const SignedContextV1StructSell = {
+      signer: '0x0124555E401547219fB024aE5F8C5101c6f7Cb24', // Replace with your signer's address
+      signature: signatureSell,
+      context: contextSell
     };
 
     // Return the SignedContextV1Struct as JSON
-    return new Response(JSON.stringify(SignedContextV1Struct), {
+    return new Response(JSON.stringify([SignedContextV1StructBuy, SignedContextV1StructSell]), {
       headers: {
         'Content-Type': 'application/json',
       },
